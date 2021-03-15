1228d3294bb570af6decb421272eba76a53282e2
yuval.keidar <keidaryu@gmail.com>
2021-03-15 18:39:35 +0200

    fix(FEC-11005): Bumper doesn't work with playadswithMSE config
    Issue: Bumper doesn't work with PlayadswithMSE.
    Solution: ad_break_start should fired once the ad start to play.
    IMA + Bumper doesn't work - should load only once we're not playing on the same video tag

--- src/bumper-middleware.js
+++ src/bumper-middleware.js
@@ -36,10 +36,7 @@ class BumperMiddleware extends BaseMiddleware {
   load(next: Function): void {
     this._nextLoad = next;
     this._context.eventManager.listenOnce(this._context.player, EventType.AD_ERROR, () => this._callNextLoad());
-    if (
-      this._context.adBreakPosition === BumperBreakType.PREROLL &&
-      !(this._context.playOnMainVideoTag() && this._context.player.getVideoElement().src)
-    ) {
+    if (this._context.adBreakPosition === BumperBreakType.PREROLL && !this._context.playOnMainVideoTag()) {
       this._context.load();
     }
     if (!(this._context.config.url && this._context.config.position.includes(BumperBreakType.PREROLL))) {
