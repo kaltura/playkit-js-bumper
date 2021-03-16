0e0d6c0da26702a34574a9cfa271765d0bdd97e8
yuval.keidar <keidaryu@gmail.com>
2021-03-16 15:40:31 +0200

    Update bumper.js

--- src/bumper.js
+++ src/bumper.js
@@ -141,10 +141,12 @@ class Bumper extends BasePlugin implements IMiddlewareProvider, IAdsControllerPr
    */
   play(): void {
     if (!this._adBreak) {
-      this.dispatchEvent(EventType.AD_LOADED, {ad: this._getAd()});
+      if (this._bumperState === BumperState.IDLE) {
+        this.dispatchEvent(EventType.AD_LOADED, {ad: this._getAd()});
+      }
+      this._adBreak = true;
+      this.dispatchEvent(EventType.AD_BREAK_START, {adBreak: this._getAdBreak()});
     }
-    this._adBreak = true;
-    this.dispatchEvent(EventType.AD_BREAK_START, {adBreak: this._getAdBreak()});
     this.load();
     this._hideElement(this._bumperCoverDiv);
     const playPromise = this._videoElement.play();
